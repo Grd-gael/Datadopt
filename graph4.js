fa53b5286fe9668fd20e5aa5786d3a10ab863d0d
N-DaSilva <158066183+N-DaSilva@users.noreply.github.com>
2024-12-02 16:37:19 +0100

    fix initialisation

--- graph4.js
+++ graph4.js
@@ -1,10 +1,12 @@
 let donneesAdoptionAccueil = []
 let donneesAdoptionOrigine = []
+let dataSet
+let currentData
 var slider = document.getElementById("range");
 var output = document.getElementById("txt-annee");
 var texte = document.getElementById("texte");
 
-function getDataSet10 (data, annee){
+function getDataSet10(data, annee) {
   let pays = []
   let nb = []
   data.forEach(function (element) {
@@ -14,42 +16,44 @@ function getDataSet10 (data, annee){
     }
   })
   console.log(nb)
-  return [pays,nb]
+  return [pays, nb]
 }
 
 fetch('pays_daccueil.json').then(response => {
   response.json().then(function (data) {
+
     donneesAdoptionAccueil = data
-    
+    dataSet = donneesAdoptionAccueil
+    currentData = getDataSet10(dataSet, slider.value);
+    texte.innerHTML = currentData[0][0] + ' ' + currentData[1][0]
+
+    fetch('pays_dorigine.json').then(response => {
+      response.json().then(function (data) {
+        donneesAdoptionOrigine = data
+      })
+    })
+
   })
 })
 
-fetch('pays_dorigine.json').then(response => {
-  response.json().then(function (data) {
-    donneesAdoptionOrigine = data
-  })
-})
 
-let dataSet = donneesAdoptionAccueil
-let currentData = getDataSet10(dataSet,slider.value);
-texte.innerHTML = currentData[0][0]+' '+currentData[1][0]
 
 document.getElementById("acc").addEventListener("click", function (e) {
   dataSet = donneesAdoptionAccueil
-  currentData = getDataSet10(dataSet,slider.value);
-  texte.innerHTML = currentData[0][0]+' '+currentData[1][0]
+  currentData = getDataSet10(dataSet, slider.value);
+  texte.innerHTML = currentData[0][0] + ' ' + currentData[1][0]
 })
 
 document.getElementById("ori").addEventListener("click", function (e) {
   dataSet = donneesAdoptionOrigine
-  currentData = getDataSet10(dataSet,slider.value);
-  texte.innerHTML = currentData[0][0]+' '+currentData[1][0]
+  currentData = getDataSet10(dataSet, slider.value);
+  texte.innerHTML = currentData[0][0] + ' ' + currentData[1][0]
 })
 
 output.innerHTML = slider.value;
 
 slider.addEventListener('input', function (e) {
   output.innerHTML = slider.value;
-  currentData = getDataSet10(dataSet,slider.value);
-  texte.innerHTML = currentData[0][0]+' '+currentData[1][0]
+  currentData = getDataSet10(dataSet, slider.value);
+  texte.innerHTML = currentData[0][0] + ' ' + currentData[1][0]
 })
