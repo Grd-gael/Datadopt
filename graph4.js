456f33502a48794afe422b15d9554b59bd82cdf6
N-DaSilva <158066183+N-DaSilva@users.noreply.github.com>
2024-12-02 17:13:02 +0100

    add graph

--- graph4.js
+++ graph4.js
@@ -1,10 +1,37 @@
-let donneesAdoptionAccueil = []
-let donneesAdoptionOrigine = []
+let donneesAdoptionAccueil
+let donneesAdoptionOrigine
 let dataSet
 let currentData
 var slider = document.getElementById("range");
 var output = document.getElementById("txt-annee");
 var texte = document.getElementById("texte");
+
+
+function dessineAxes(id,a_length,o_height){
+  const svg = document.querySelector(id);
+  const abscisse = `<path class="axe-horizontal" style="stroke:black" d="M 0  ${o_height} L ${a_length} ${o_height}"/>`;
+  const ordonnee = `<path class="axe-vertical" style="stroke:black" d="M 0 0 L 0 ${o_height}"/>` ;
+
+  svg.innerHTML = `${abscisse} ${ordonnee}`;
+}
+
+function dessineBarre(id, n, height, width, margin, color, vbmax){
+  const svg = document.getElementById(id);
+  var barre = `<path class="barre" style="fill:${color};" d="M ${margin*n} ${vbmax} L ${margin*n} ${vbmax-height} L ${width+margin*n} ${vbmax-height} L ${width+margin*n} ${vbmax} L ${margin*n} ${vbmax}"/>`
+
+  svg.innerHTML += barre;
+}
+
+function dessineHistogramme(id, abscisse, ordonnee, barres, b_width, margin, color){
+  document.getElementById(id).innerHTML= ''
+  n = 0
+  barres.forEach(function (barre){
+    dessineBarre(id, n, barre, b_width, margin, color, ordonnee);
+    n++;
+  })
+  dessineAxes(id,abscisse,ordonnee);
+}
+
 
 function getDataSet10(data, annee) {
   let pays = []
@@ -25,14 +52,14 @@ fetch('pays_daccueil.json').then(response => {
     donneesAdoptionAccueil = data
     dataSet = donneesAdoptionAccueil
     currentData = getDataSet10(dataSet, slider.value);
-    texte.innerHTML = currentData[0][0] + ' ' + currentData[1][0]
 
-    fetch('pays_dorigine.json').then(response => {
-      response.json().then(function (data) {
-        donneesAdoptionOrigine = data
-      })
-    })
+    dessineHistogramme('chart',1000,1000,currentData[1],100,200,'blue')
+  })
+})
 
+fetch('pays_dorigine.json').then(response => {
+  response.json().then(function (data) {
+    donneesAdoptionOrigine = data
   })
 })
 
@@ -41,13 +68,15 @@ fetch('pays_daccueil.json').then(response => {
 document.getElementById("acc").addEventListener("click", function (e) {
   dataSet = donneesAdoptionAccueil
   currentData = getDataSet10(dataSet, slider.value);
-  texte.innerHTML = currentData[0][0] + ' ' + currentData[1][0]
+
+  dessineHistogramme('chart',1000,1000,currentData[1],100,200,'blue')
 })
 
 document.getElementById("ori").addEventListener("click", function (e) {
   dataSet = donneesAdoptionOrigine
   currentData = getDataSet10(dataSet, slider.value);
-  texte.innerHTML = currentData[0][0] + ' ' + currentData[1][0]
+
+  dessineHistogramme('chart',1000,1000,currentData[1],100,200,'blue')
 })
 
 output.innerHTML = slider.value;
@@ -55,5 +84,6 @@ output.innerHTML = slider.value;
 slider.addEventListener('input', function (e) {
   output.innerHTML = slider.value;
   currentData = getDataSet10(dataSet, slider.value);
-  texte.innerHTML = currentData[0][0] + ' ' + currentData[1][0]
+
+  dessineHistogramme('chart',1000,1000,currentData[1],100,200,'blue')
 })
