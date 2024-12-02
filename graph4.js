5e09e98827659c44eb1f374a409dbd0279fc706b
N-DaSilva <158066183+N-DaSilva@users.noreply.github.com>
2024-12-02 15:17:02 +0100

    graphique 4

---
+++ graph4.js
@@ -0,0 +1,55 @@
+let donneesAdoptionAccueil = []
+let donneesAdoptionOrigine = []
+var slider = document.getElementById("range");
+var output = document.getElementById("txt-annee");
+var texte = document.getElementById("texte");
+
+function getDataSet10 (data, annee){
+  let pays = []
+  let nb = []
+  data.forEach(function (element) {
+    if (data.indexOf(element) < 10) {
+      pays.push(element['pays'])
+      nb.push(element[annee])
+    }
+  })
+  console.log(nb)
+  return [pays,nb]
+}
+
+fetch('pays_daccueil.json').then(response => {
+  response.json().then(function (data) {
+    donneesAdoptionAccueil = data
+    
+  })
+})
+
+fetch('pays_dorigine.json').then(response => {
+  response.json().then(function (data) {
+    donneesAdoptionOrigine = data
+  })
+})
+
+let dataSet = donneesAdoptionAccueil
+let currentData = getDataSet10(dataSet,slider.value);
+texte.innerHTML = currentData[0][0]+' '+currentData[1][0]
+
+document.getElementById("acc").addEventListener("click", function (e) {
+  dataSet = donneesAdoptionAccueil
+  currentData = getDataSet10(dataSet,slider.value);
+  texte.innerHTML = currentData[0][0]+' '+currentData[1][0]
+})
+
+document.getElementById("ori").addEventListener("click", function (e) {
+  dataSet = donneesAdoptionOrigine
+  currentData = getDataSet10(dataSet,slider.value);
+  texte.innerHTML = currentData[0][0]+' '+currentData[1][0]
+})
+
+output.innerHTML = slider.value;
+
+slider.addEventListener('input', function (e) {
+  output.innerHTML = slider.value;
+  currentData = getDataSet10(dataSet,slider.value);
+  texte.innerHTML = currentData[0][0]+' '+currentData[1][0]
+})
