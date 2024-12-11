723de04997deb0e3cd2fa167ae68e2130a3746b5
N-DaSilva <158066183+N-DaSilva@users.noreply.github.com>
2024-12-11 23:28:30 +0100

    fix graph 3 display

--- graph3.js
+++ graph3.js
@@ -4,18 +4,6 @@ const rects = document.querySelectorAll('.section3 rect')
 const total = document.querySelector('.nb_adoptions')
 let dataPodium
 
-const animationPodium = [
-    { transform: "translateY(-20%)scale(1,0)" },
-    { transform: "translateY(0%)scale(1,1)" },
-  ];
-  
-  const animationNumbers = [
-    { opacity: 0 },
-    { opacity: 0 },
-    { opacity: 0 },
-    { opacity: 1 },
-  ];
-  
 function getPodium(data, annee) {
     let traitement = data
         .map(item => ({
@@ -25,32 +13,31 @@ function getPodium(data, annee) {
         .filter(item => !isNaN(item.nb))
         .sort((a, b) => b.nb - a.nb)
         .slice(0, 3);
+    let tmp = traitement.pop()
+    traitement.unshift(tmp)
+    console.log(traitement)
     return traitement
 }
 
 function displayData(data) {
-    let podium = getPodium(data,slider.value)
-    let n=0
-    labels.forEach(function (label){
+    let podium = getPodium(data, slider.value)
+    let n = 0
+    labels.forEach(function (label) {
         label.innerHTML = podium[n].pays
-        label.animate(animationNumbers,timing)
         n++
     })
-    n=0
-    values.forEach(function (value){
+    n = 0
+    values.forEach(function (value) {
         value.innerHTML = podium[n].nb
-        value.animate(animationNumbers,timing)
         n++
     })
-
-    rects.forEach(rect => rect.animate(animationPodium,timing))
 }
 
-function totalAdoptions(data){
+function totalAdoptions(data) {
     let total = 0
-    data.forEach(function (pays){
-        if (!isNaN(parseInt(pays['nb_adoptions_'+slider.value]))) {
-            total+= parseInt(pays['nb_adoptions_'+slider.value])
+    data.forEach(function (pays) {
+        if (!isNaN(parseInt(pays['nb_adoptions_' + slider.value]))) {
+            total += parseInt(pays['nb_adoptions_' + slider.value])
         }
     })
     return total
@@ -60,11 +47,11 @@ fetch('adoption-1979-2022-origine-par-pays.json').then(response => {
     response.json().then(function (data) {
         dataPodium = data
         displayData(data)
-        total.innerHTML=totalAdoptions(data)
+        total.innerHTML = totalAdoptions(data)
     })
 })
 
-slider.addEventListener('input',function (e) {
+slider.addEventListener('input', function (e) {
     displayData(dataPodium)
-    total.innerHTML=totalAdoptions(dataPodium)
+    total.innerHTML = totalAdoptions(dataPodium)
 })
